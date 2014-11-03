4459dacc25a2f35583fd3d12bddf0d8589a39c37
Samuel Cabrero <samuelcabrero@kernevil.me>
2014-11-03 13:20:10 +0100

    TODO SIGN-OFF s4:dsdb/util_samr: Fix bug domain users cannot join computers to domain
    Implement necessary operations and checks to allow domain users to join
    computers to the domain. It is necessary to grant the SEC_PRIV_MACHINE_ACCOUNT
    to "Authenticated Users" group.
    
    TODO Signed-off-by: Samuel Cabrero <samuelcabrero@kernevil.me>

--- source4/dsdb/common/util_samr.c
+++ source4/dsdb/common/util_samr.c
@@ -27,8 +27,204 @@
 #include "dsdb/common/util.h"
 #include "../libds/common/flags.h"
 #include "libcli/security/security.h"
-
 #include "libds/common/flag_mapping.h"
+#include "param/param.h"
+#include "libcli/ldap/ldap_ndr.h"
+#include "librpc/gen_ndr/ndr_security.h"
+#include "librpc/gen_ndr/auth.h"
+
+/**
+ * If the client creating the machine account does not have the
+ * SEC_ADS_CREATE_CHILD access right on the container object and the role is
+ * DC, checks for the SEC_PRIV_MACHINE_ACCOUNT privilege.
+ */
+static NTSTATUS dsdb_add_machine_is_privileged(TALLOC_CTX *tmp_ctx,
+					       struct ldb_context *ldb,
+					       struct ldb_dn *container_dn,
+					       bool *privileged)
+{
+	struct auth_session_info *sinfo = NULL;
+	int ret;
+
+	sinfo = talloc_get_type_abort(ldb_get_opaque(ldb, "sessionInfo"), struct auth_session_info);
+
+	ret = dsdb_check_access_on_dn(ldb, tmp_ctx, container_dn, sinfo->security_token,
+				      SEC_ADS_CREATE_CHILD, NULL);
+	if (ret == LDB_ERR_INSUFFICIENT_ACCESS_RIGHTS) {
+		struct loadparm_context *lp_ctx;
+		lp_ctx = talloc_get_type_abort(ldb_get_opaque(ldb, "loadparm"), 
+					       struct loadparm_context);
+
+		/* Check role, if not DC return access denied */
+		if (lpcfg_server_role(lp_ctx) != ROLE_ACTIVE_DIRECTORY_DC) {
+			*privileged = false;
+			return NT_STATUS_ACCESS_DENIED;
+		}
+
+		*privileged = security_token_has_privilege(sinfo->security_token,
+							   SEC_PRIV_MACHINE_ACCOUNT);
+		return NT_STATUS_ACCESS_DENIED;
+	}
+	if (ret != LDB_SUCCESS) {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	return NT_STATUS_OK;
+}
+
+/**
+ * When the machine account is created using the SEC_PRIV_MACHINE_ACCOUNT
+ * privilege check the quota. If the attribute does not exists, the default
+ * value is used (10).
+ */
+static NTSTATUS dsdb_add_machine_privileged_check_quota(TALLOC_CTX *tmp_ctx,
+							struct ldb_context *ldb)
+{
+	struct auth_session_info *sinfo = NULL;
+	struct ldb_dn *base_dn;
+	struct ldb_result *res;
+	struct ldb_message *msg;
+	struct dom_sid *sid;
+	int quota;
+	int ret;
+	const char *quota_attrs[] = {
+		"ms-DS-MachineAccountQuota",
+		NULL,
+	};
+	const char *creator_attrs[] = {
+		"mS-DS-CreatorSID",
+		NULL,
+	};
+
+	/* Get the quota */
+	base_dn = ldb_get_default_basedn(ldb);
+	if (base_dn == NULL) {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	ret = ldb_search(ldb, tmp_ctx, &res, base_dn, LDB_SCOPE_BASE,
+			 quota_attrs, "(objectClass=domain)");
+	if (ret != LDB_SUCCESS) {
+		return NT_STATUS_INTERNAL_DB_CORRUPTION;
+	}
+	if (res->count != 1) {
+		return NT_STATUS_INTERNAL_DB_CORRUPTION;
+	}
+	msg = res->msgs[0];
+	quota = ldb_msg_find_attr_as_int(msg, "ms-DS-MachineAccountQuota", 10);
+	talloc_free(res);
+
+	/* Get client SID */
+	sinfo = talloc_get_type_abort(ldb_get_opaque(ldb, "sessionInfo"), struct auth_session_info);
+	sid = &sinfo->security_token->sids[0];
+	if (sid == NULL) {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	/* Check quota not exceeded by the client */
+	ret = ldb_search(ldb, tmp_ctx, &res, base_dn, LDB_SCOPE_SUBTREE,
+			 creator_attrs, "(mS-DS-CreatorSID=%s)",
+			 ldap_encode_ndr_dom_sid(tmp_ctx, sid));
+	if (ret != LDB_SUCCESS) {
+		return NT_STATUS_INTERNAL_DB_CORRUPTION;
+	}
+	if (res->count >= quota) {
+		DEBUG(0, ("Failed to create account, quota exceeded\n"));
+		return NT_STATUS_ACCESS_DENIED;
+	}
+
+	return NT_STATUS_OK;
+}
+
+/**
+ * When the machine account is created using the SEC_PRIV_MACHINE_ACCOUNT
+ * privilege the resulting default security descriptor of the object has to
+ * be fixed changing the owner to Domain Admins and the group to Domain Users.
+ */
+static NTSTATUS dsdb_add_machine_privileged_set_sd(TALLOC_CTX *tmp_ctx,
+						   struct ldb_context *ldb,
+						   struct ldb_dn *account_dn)
+{
+	struct security_descriptor *sd;
+	const struct dom_sid *domain_sid;
+	int ret;
+	DATA_BLOB data;
+	enum ndr_err_code ndr_err;
+	struct ldb_request *req;
+	struct ldb_message *msg;
+	const char *sd_attrs[] = {
+		"nTSecurityDescriptor",
+		NULL
+	};
+
+	domain_sid = samdb_domain_sid(ldb);
+	if (domain_sid == NULL) {
+		return NT_STATUS_INTERNAL_DB_CORRUPTION;
+	}
+
+	ret = dsdb_search_one(ldb, tmp_ctx, &msg, account_dn,
+			      LDB_SCOPE_BASE, sd_attrs, 0, NULL);
+	if (ret != LDB_SUCCESS) {
+		return NT_STATUS_INTERNAL_DB_CORRUPTION;
+	}
+
+	ret = dsdb_get_sd_from_ldb_message(ldb, tmp_ctx, msg, &sd);
+	if (ret != LDB_SUCCESS) {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	sd->group_sid = dom_sid_add_rid(tmp_ctx, domain_sid, DOMAIN_RID_USERS);
+	sd->owner_sid = dom_sid_add_rid(tmp_ctx, domain_sid, DOMAIN_RID_ADMINS);
+	ndr_err = ndr_push_struct_blob(&data, tmp_ctx, sd,
+				       (ndr_push_flags_fn_t)ndr_push_security_descriptor);
+	if (!NDR_ERR_CODE_IS_SUCCESS(ndr_err)) {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	msg = ldb_msg_new(tmp_ctx);
+	if (msg == NULL) {
+		return NT_STATUS_NO_MEMORY;
+	}
+	msg->dn = ldb_dn_copy(tmp_ctx, account_dn);
+
+	ret = ldb_msg_add_value(msg, "nTSecurityDescriptor", &data, NULL);
+	if (ret != LDB_SUCCESS) {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+	msg->elements[0].flags = LDB_FLAG_MOD_REPLACE;
+
+	ret = ldb_msg_sanity_check(ldb, msg);
+	if (ret != LDB_SUCCESS) {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	ret = ldb_build_mod_req(&req, ldb, tmp_ctx,
+				msg,
+				NULL,
+				NULL,
+				ldb_op_default_callback,
+				NULL);
+	ret = ldb_request_add_control(req, LDB_CONTROL_AS_SYSTEM_OID,
+				      false, NULL);
+	if (ret != LDB_SUCCESS) {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	ret = ldb_request(ldb, req);
+	if (ret == LDB_SUCCESS) {
+		ret = ldb_wait(req->handle, LDB_WAIT_ALL);
+		if (ret != LDB_SUCCESS) {
+			DEBUG(0, ("Failed to set nTSecurityDescriptor on %s: %s",
+				  ldb_dn_get_linearized(account_dn),
+				  ldb_errstring(ldb)));
+			return NT_STATUS_INTERNAL_ERROR;
+		}
+	} else {
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	return NT_STATUS_OK;
+}
 
 /* Add a user, SAMR style, including the correct transaction
  * semantics.  Used by the SAMR server and by pdb_samba4 */
@@ -41,6 +237,7 @@ NTSTATUS dsdb_add_user(struct ldb_context *ldb,
 		       struct ldb_dn **dn)
 {
 	const char *name;
+	struct ldb_request *req;
 	struct ldb_message *msg;
 	int ret;
 	const char *container, *obj_class=NULL;
@@ -56,6 +253,7 @@ NTSTATUS dsdb_add_user(struct ldb_context *ldb,
 	uint32_t user_account_control;
 	struct ldb_dn *account_dn;
 	struct dom_sid *account_sid;
+	bool machine_account_privilege = false;
 
 	TALLOC_CTX *tmp_ctx = talloc_new(mem_ctx);
 	NT_STATUS_HAVE_NO_MEMORY(tmp_ctx);
@@ -111,6 +309,9 @@ NTSTATUS dsdb_add_user(struct ldb_context *ldb,
 		obj_class = "user";
 		user_account_control = UF_NORMAL_ACCOUNT;
 	} else if (acct_flags == ACB_WSTRUST) {
+		NTSTATUS status;
+		struct ldb_dn *container_dn;
+
 		if (cn_name[cn_name_len - 1] != '$') {
 			ldb_transaction_cancel(ldb);
 			return NT_STATUS_FOOBAR;
@@ -120,6 +321,39 @@ NTSTATUS dsdb_add_user(struct ldb_context *ldb,
 		obj_class = "computer";
 		user_account_control = UF_WORKSTATION_TRUST_ACCOUNT;
 
+		container_dn = ldb_dn_copy(msg, ldb_get_default_basedn(ldb));
+		if (container_dn == NULL) {
+			ldb_transaction_cancel(ldb);
+			talloc_free(tmp_ctx);
+			return NT_STATUS_NO_MEMORY;
+		}
+		if (!ldb_dn_add_child_fmt(container_dn, "%s", container)) {
+			ldb_transaction_cancel(ldb);
+			talloc_free(tmp_ctx);
+			return NT_STATUS_INTERNAL_ERROR;
+		}
+
+		status = dsdb_add_machine_is_privileged(tmp_ctx, ldb, container_dn,
+							&machine_account_privilege);
+		if (NT_STATUS_EQUAL(status, NT_STATUS_ACCESS_DENIED)) {
+			if (!machine_account_privilege) {
+				ldb_transaction_cancel(ldb);
+				talloc_free(tmp_ctx);
+				return NT_STATUS_ACCESS_DENIED;
+			}
+
+			/* Check quota for privileged operation */
+			status = dsdb_add_machine_privileged_check_quota(tmp_ctx, ldb);
+			if (NT_STATUS_IS_ERR(status)) {
+				ldb_transaction_cancel(ldb);
+				talloc_free(tmp_ctx);
+				return status;
+			}
+		} else if (NT_STATUS_IS_ERR(status)) {
+			ldb_transaction_cancel(ldb);
+			talloc_free(tmp_ctx);
+			return status;
+		}
 	} else if (acct_flags == ACB_SVRTRUST) {
 		if (cn_name[cn_name_len - 1] != '$') {
 			ldb_transaction_cancel(ldb);
@@ -172,8 +406,61 @@ NTSTATUS dsdb_add_user(struct ldb_context *ldb,
 		}
 	}
 
+	ret = ldb_msg_sanity_check(ldb, msg);
+	if (ret != LDB_SUCCESS) {
+		ldb_transaction_cancel(ldb);
+		talloc_free(tmp_ctx);
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	ret = ldb_build_add_req(&req, ldb, tmp_ctx, msg, NULL, NULL,
+				ldb_op_default_callback, NULL);
+	if (ret != LDB_SUCCESS) {
+		ldb_transaction_cancel(ldb);
+		talloc_free(tmp_ctx);
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	if (machine_account_privilege) {
+		struct auth_session_info *sinfo = NULL;
+		struct dom_sid *creator_sid = NULL;
+		DATA_BLOB data;
+		enum ndr_err_code ndr_err;
+
+		sinfo = talloc_get_type_abort(ldb_get_opaque(ldb, "sessionInfo"), struct auth_session_info);
+
+		/* Add msDS-Creator-Sid to message */
+		creator_sid = &sinfo->security_token->sids[0];
+		ndr_err = ndr_push_struct_blob(&data, mem_ctx, creator_sid,
+					       (ndr_push_flags_fn_t)ndr_push_dom_sid);
+		if (!NDR_ERR_CODE_IS_SUCCESS(ndr_err)) {
+			ldb_transaction_cancel(ldb);
+			talloc_free(tmp_ctx);
+			return NT_STATUS_INTERNAL_ERROR;
+		}
+		ret = ldb_msg_add_value(msg, "mS-DS-CreatorSID", &data, NULL);
+		if (ret != LDB_SUCCESS) {
+			ldb_transaction_cancel(ldb);
+			talloc_free(tmp_ctx);
+			return NT_STATUS_INTERNAL_ERROR;
+		}
+
+		ret = ldb_request_add_control(req, LDB_CONTROL_AS_SYSTEM_OID,
+					      false, NULL);
+		if (ret != LDB_SUCCESS) {
+			ldb_transaction_cancel(ldb);
+			talloc_free(tmp_ctx);
+			return NT_STATUS_INTERNAL_ERROR;
+		}
+	}
+
 	/* create the user */
-	ret = ldb_add(ldb, msg);
+	ret = ldb_request(ldb, req);
+	if (ret == LDB_SUCCESS) {
+		ret = ldb_wait(req->handle, LDB_WAIT_ALL);
+	}
+	talloc_free(req);
+
 	switch (ret) {
 	case LDB_SUCCESS:
 		break;
@@ -202,6 +489,17 @@ NTSTATUS dsdb_add_user(struct ldb_context *ldb,
 	}
 
 	account_dn = msg->dn;
+
+	if (machine_account_privilege) {
+		NTSTATUS status;
+		status = dsdb_add_machine_privileged_set_sd(tmp_ctx, ldb,
+							    account_dn);
+		if (NT_STATUS_IS_ERR(status)) {
+			ldb_transaction_cancel(ldb);
+			talloc_free(tmp_ctx);
+			return status;
+		}
+	}
 
 	/* retrieve the sid and account control bits for the user just created */
 	ret = dsdb_search_one(ldb, tmp_ctx, &msg,
