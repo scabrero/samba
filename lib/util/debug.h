233694711319aa9b52507e9f3bb83236700f59f2
Samuel Cabrero <scabrero@suse.de>
2017-08-08 10:23:36 +0200

    s4:dfsr: Add the DFS replication service
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>

--- lib/util/debug.h
+++ lib/util/debug.h
@@ -103,6 +103,7 @@ bool dbghdr( int level, const char *location, const char *func);
 #define DBGC_DSDB_TXN_AUDIT_JSON	35
 #define DBGC_DSDB_GROUP_AUDIT	36
 #define DBGC_DSDB_GROUP_AUDIT_JSON	37
+#define DBGC_DFSR		38
 
 /* So you can define DBGC_CLASS before including debug.h */
 #ifndef DBGC_CLASS
