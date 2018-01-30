57fb796b7690b8f87e3f6ac80f4faec8d74a1cf9
Samuel Cabrero <scabrero@suse.de>
2018-01-30 10:29:59 +0100

    s4: dfsr: Uncompress staged files to the installing directory
    Signed-off-by: Samuel Cabrero <scabrero@samba.org>

--- source3/smbd/dfsr_meet.c
+++ source3/smbd/dfsr_meet.c
@@ -28,6 +28,8 @@
 #include "source4/lib/messaging/irpc.h"
 #include "gen_ndr/ndr_frsblobs.h"
 #include "dfsr/dfsr_db.h"
+#include "system/filesys.h"
+#include "lib/compression/lzhuff_xpress.h"
 
 #undef DBGC_CLASS
 #define DBGC_CLASS DBGC_DFSR_MEET
@@ -38,6 +40,176 @@ struct dfsr_meet_state {
 	pid_t parent_pid;
 	struct dfsr_db *db_ctx;
 };
+
+static NTSTATUS dfsr_meet_get_installing_path(TALLOC_CTX *mem_ctx,
+		const char *installing_dir,
+		const struct frstrans_Update *update,
+		char **path)
+{
+	struct GUID_txt_buf tmp_buf1;
+
+	if (path == NULL || update == NULL) {
+		return NT_STATUS_INVALID_PARAMETER;
+	}
+
+	*path = talloc_asprintf(mem_ctx, "%s/%s_%lu", installing_dir,
+			GUID_buf_string(&update->gsvn_db_guid, &tmp_buf1),
+			update->gsvn_version);
+	if (*path == NULL) {
+		return NT_STATUS_NO_MEMORY;
+	}
+
+	return NT_STATUS_OK;
+}
+
+static NTSTATUS dfsr_meet_uncompress_staged(TALLOC_CTX *mem_ctx,
+		const char *input,
+		const char *output)
+{
+	TALLOC_CTX *tmp_ctx;
+	NTSTATUS status;
+	int fd_in;
+	int fd_out;
+	char buffer[8192];
+	ssize_t nread;
+	ssize_t nwritten;
+	off_t offset;
+
+	DBG_DEBUG("Uncompressing '%s' to '%s'\n", input, output);
+
+	fd_in = 0;
+	fd_out = 0;
+
+	tmp_ctx = talloc_new(mem_ctx);
+	if (tmp_ctx == NULL) {
+		return NT_STATUS_NO_MEMORY;
+	}
+
+	fd_in = open(input, O_RDONLY);
+	if (fd_in == -1) {
+		DBG_ERR("Failed to open staged file %s: %s\n",
+			input, strerror(errno));
+		status = map_nt_error_from_unix(errno);
+		goto out;
+	}
+
+	fd_out = open(output, O_RDWR | O_CREAT | O_TRUNC);
+	if (fd_out == -1) {
+		DBG_ERR("Failed to open install file %s: %s\n",
+			output, strerror(errno));
+		status = map_nt_error_from_unix(errno);
+		goto out;
+	}
+
+	/*
+	 * [MS-FRS2] 3.2.4.1.14.2 The compressed data stream starts with
+	 * a header ('F', 'R', 'S', 'X')
+	 */
+	nread = read(fd_in, buffer, 4);
+	if (nread != 4) {
+		DBG_ERR("Failed to read staged file header: %s\n",
+			strerror(errno));
+		status = map_nt_error_from_unix(errno);
+		goto out;
+	}
+	if (memcmp(buffer, "FRSX", 4) != 0) {
+		DBG_ERR("Invalid staged file signature\n");
+		status = NT_STATUS_FILE_CORRUPT_ERROR;
+		goto out;
+	}
+	offset = 4;
+
+	/*
+	 * [MS-FRS2] 3.2.4.1.14.2 The header is followed by a series of one
+	 * or more XPRESS blocks. The maximum XPRESS block size is 8192
+	 * bytes and the XPRESS block header is 12 bytes.
+	 */
+	while ((nread = read(fd_in, buffer, 8192)) > 0) {
+		DATA_BLOB compressed;
+		DATA_BLOB uncompressed;
+		struct dfsr_xpress_block block;
+		enum ndr_err_code ndr_err;
+
+		if (nread < 0) {
+			DBG_ERR("Failed to read staged file: %s\n",
+				strerror(errno));
+			status = map_nt_error_from_unix(errno);
+			goto out;
+		}
+
+		compressed = data_blob_const(buffer, nread);
+		ndr_err = ndr_pull_struct_blob(&compressed, tmp_ctx, &block,
+			(ndr_pull_flags_fn_t)ndr_pull_dfsr_xpress_block);
+		if (!NDR_ERR_CODE_IS_SUCCESS(ndr_err)) {
+			DBG_ERR("Failed to pull XPRESS blob: %s\n",
+				ndr_errstr(ndr_err));
+			status = ndr_map_error2ntstatus(ndr_err);
+			goto out;
+		}
+
+		offset += (block.compressed_size + 12 /* header size */);
+
+		/* Set the file offset to the beginning of the next block */
+		if (lseek(fd_in, offset, SEEK_SET) < 0) {
+			int saved_errno = errno;
+			DBG_ERR("lseek() error: %s\n", strerror(saved_errno));
+			status = map_nt_error_from_unix(saved_errno);
+			goto out;
+		}
+
+		/*
+		 * [MS-FRS2] 2.2.1.4.15 If the value of the block compressed
+		 * size field is less than the value of the block uncompressed
+		 * size field, then the data has been compressed
+		 */
+		if (block.compressed_size < block.uncompressed_size) {
+			ssize_t usize;
+			uncompressed = data_blob_talloc_zero(tmp_ctx,
+					block.uncompressed_size);
+			usize = lzhuff_xpress_decompress(block.data.data,
+					block.data.length,
+					uncompressed.data,
+					uncompressed.length);
+			if (usize != block.uncompressed_size) {
+				DBG_ERR("Uncompressed block size does not "
+					"match\n");
+				status = NT_STATUS_FILE_CORRUPT_ERROR;
+				goto out;
+			}
+		} else {
+			uncompressed = block.data;
+		}
+
+		/* Write the uncompressed block to installing file */
+		nwritten = write(fd_out, uncompressed.data,
+				uncompressed.length);
+		if (nwritten != uncompressed.length) {
+			DBG_ERR("Failed to write on install file %s: %s\n",
+				output, strerror(errno));
+			status = map_nt_error_from_unix(errno);
+			goto out;
+		}
+
+		/* Free the uncompressed blob and continue to next block */
+		if (block.compressed_size < block.uncompressed_size) {
+			data_blob_free(&uncompressed);
+		}
+	}
+
+	status = NT_STATUS_OK;
+
+out:
+	if (fd_in > 0) {
+		close(fd_in);
+	}
+	if (fd_out > 0) {
+		close(fd_out);
+	}
+
+	TALLOC_FREE(tmp_ctx);
+
+	return status;
+}
 
 static NTSTATUS dfsr_meet_store(TALLOC_CTX *mem_ctx,
 				struct dfsr_db *db_ctx,
@@ -99,13 +271,62 @@ static NTSTATUS dfsr_meet_install_update_internal(TALLOC_CTX *mem_ctx,
 {
 	TALLOC_CTX *tmp_ctx;
 	NTSTATUS status;
+	char *installing_path;
 
 	tmp_ctx = talloc_new(mem_ctx);
 	if (tmp_ctx == NULL) {
 		return NT_STATUS_NO_MEMORY;
 	}
 
+	/* Check if this update is a tombstone pertaining to a deletion */
+	if (update->present == 0) {
+		/* Store the tombstone */
+		goto store;
+	}
+
+	/*
+	 * [MS-FRS2] Section 3.2.4.1.14 File data is transferred over the wire
+	 * in a format that is composed of two layers:
+	 * 1. A stream of file data that consists of a custom marshaled format
+	 * 2. An encapsulation of the marshaled file data stream using the
+	 *    compressed data format. Even if the marshaled file data stream
+	 *    is not compressed by the server, it is still encapsulated using
+	 *    the compressed data format
+	 */
+
+	/* Build the installing file path */
+	status = dfsr_meet_get_installing_path(tmp_ctx, installing_dir, update,
+			&installing_path);
+	if (!NT_STATUS_IS_OK(status)) {
+		DBG_ERR("Failed to build installing path: %s\n",
+			nt_errstr(status));
+		goto out;
+	}
+
+	/* Uncompress the staged file to installing area */
+	status = dfsr_meet_uncompress_staged(tmp_ctx, staged_file,
+			installing_path);
+	if (!NT_STATUS_IS_OK(status)) {
+		DBG_ERR("Failed to uncompress staged update: %s\n",
+			nt_errstr(status));
+		goto out;
+	}
+
+store:
 	status = dfsr_meet_store(tmp_ctx, state->db_ctx, update);
+
+out:
+	if (installing_path != NULL && strlen(installing_path) > 0) {
+		int rc = unlink(installing_path);
+		if (rc == -1) {
+			if (errno != ENOENT) {
+				DBG_ERR("Failed to delete installing file "
+					"'%s': %s\n",
+					installing_path,
+					strerror(errno));
+			}
+		}
+	}
 
 	TALLOC_FREE(tmp_ctx);
 
